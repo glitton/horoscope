fa3bcc8bb696d2941bb0bb726b4112cd2de008ea
Generosa Litton <generosa.litton@yahoo.com>
2017-08-02 21:34:13 -0700

    wip loader

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-// When page loads, display console
+// When page loads, display console log message
 $(document).ready(function() {
     console.log( "page is loaded" );
-  // Hide loader div first
-  $("#hidden").hide();  
+  // Hide preloader div 
+  $("#preloader").hide();  
 
 // This function uses POST request with ajax, jquery to the Horoscope-API
   function showHoroscope(sign){  
@@ -18,30 +18,21 @@ $(document).ready(function() {
       }
     });
   }    
-    // This function enables a loader to display on the page after the user 
-    // clicks the submit button
-    // function showLoader() {
-    //   // This shows the loader on the page
-    //   $("#hidden").show(); 
-    //   // The setTimeout adds 3 seconds then calls showPage
-    //   var myVar = setTimeout(showPage, 3000);
-    // }
-    // // The showPage function hides the loader using display none
-    // function showPage() {
-    //   document.getElementById("loader").style.display = "none";
-    // }
-    // makes sure the whole site is loaded
-    function showHideLoader(){
-      $(window).load(function() {
+
+    function showHideLoader(sign){
+      console.log('loader called')
+      // makes sure the whole site is loaded
+        // display the preloader div
+        $("#preloader").show();
         // will first fade out the loading animation
         $("#status").delay(3000).fadeOut();
         // will fade out the whole DIV that covers the website after 5 seconds
         $("#preloader").delay(5000).fadeOut("slow");
-      })
-    }
+      };
 
-  // Using an event listener which is submit button
-  //Return zodiac sign depending on user input of month and date
+  // Using an event listener which is the submit button
+  //Return zodiac sign depending on user input of month and date, 
+  // call showHideLoader function
     document.getElementById("submit").addEventListener("click", function(){   
     // Display zodiac sign, the date, and horoscope for the day
       // Values needed to return zodiac sign, need to be inside the function
@@ -54,56 +45,66 @@ $(document).ready(function() {
 
       console.log("You were born on " + month + ' ' + day); 
       
-
       if((month == 'January' && day <= 20) || (month == 'December' && day >=22)) { sign = getZodiac[0];
         // zodiacImg = 'img/capricorn-2-128.png';
         console.log(sign);
-        showLoader();
+        showHideLoader(sign);
         showHoroscope(sign);
 
       } else if ((month == 'January' && day >= 21) || (month == 'February' && day <= 18)) { sign = getZodiac[1];
         // zodiacImg = 'img/capricorn-2-128.png';
         console.log(sign);
+        showHideLoader(sign);
         showHoroscope(sign);
 
       } else if((month == 'February' && day >= 19) || (month == 'March' && day <= 20)) { sign = getZodiac[2];
         console.log(sign);
+        showHideLoader(sign);
         showHoroscope(sign);
 
       } else if((month == 'March' && day >= 21) || (month == 'April' && day <= 20)) { sign = getZodiac[3];
         console.log(sign);
+        showHideLoader(sign);
         showHoroscope(sign);
 
       } else if((month == 'April' && day >= 21) || (month == 'May' && day <= 20)) { sign = getZodiac[4];
         console.log(sign);
+        showHideLoader(sign);
         showHoroscope(sign);
       
       } else if((month == 'May' && day >= 21) || (month == 'June' && day <= 20)) { sign = getZodiac[5];
         console.log(sign);
+        showHideLoader(sign);
         showHoroscope(sign);
 
       } else if((month ==  'June' && day >= 22) || (month == 'July' && day <= 22)) { sign = getZodiac[6];
         console.log(sign);
+        showHideLoader(sign);
         showHoroscope(sign);
 
       } else if((month == 'July' && day >= 23) || (month == 'August' && day <= 23)) { sign = getZodiac[7];
         console.log(sign);
+        showHideLoader(sign);
         showHoroscope(sign);
 
       } else if((month == 'August' && day >= 24) || (month == 'September' && day <= 23)) { sign = getZodiac[8];
         console.log(sign);
+        showHideLoader(sign);
         showHoroscope(sign);
   
       } else if((month == 'September' && day >= 24) || (month == 'October' && day <= 23)) { sign = getZodiac[9];
         console.log(sign);
+        showHideLoader(sign);
         showHoroscope(sign);
   
       } else if((month == 'October' && day >= 24) || (month == 'November' && day <= 22)) { sign = getZodiac[10];
         console.log(sign);
+        showHideLoader(sign);
         showHoroscope(sign);
       
       } else if((month == 'November' && day >= 23) || (month == 'December' && day <= 21)) { sign = getZodiac[11];
         console.log(sign);
+        showHideLoader(sign);
         showHoroscope(sign);
       }
       // Add the user sign on to the page
